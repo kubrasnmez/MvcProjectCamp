61c6a0d0415aaea96f8e4f126b69123936eb2408
kubrasnmez <sonmezkbra@gmail.com>
2021-04-29 03:10:57 +0300

    CategoryUpdated

--- MvcProjectCamp/Controllers/CategoryController.cs
+++ MvcProjectCamp/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,21 @@ namespace MvcProjectCamp.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+
+        CategoryManager categoryManager = new CategoryManager();
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = categoryManager.GetAll();
+            return View(categoryValues);
+        }
+        public ActionResult AddCategory(Category category)
+        {
+            return RedirectToAction("GetCategoryList");
+        }
+
     }
 }
