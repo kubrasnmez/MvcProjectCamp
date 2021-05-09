bfb96ea233d8945bf73b8dae69195b0f533184eb
kubrasnmez <sonmezkbra@gmail.com>
2021-05-09 15:25:22 +0300

    temanın tüklenmesi ve kategori ekleme

---
+++ MvcProjectCamp/Controllers/AdminCategoryController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Entity_Framework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        // GET: AdminCategory
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var categoryValues = categoryManager.GetAll();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result = categoryValidator.Validate(category);
+            if (result.IsValid)
+            {
+                categoryManager.Add(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
