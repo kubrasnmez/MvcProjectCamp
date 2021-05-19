0d5b87d196c03315cbb93bd26d4a2fccc9cf614c
kubrasnmez <sonmezkbra@gmail.com>
2021-05-19 12:38:24 +0300

    Heading

--- MvcProjectCamp/Controllers/HeadingsController.cs
+++ MvcProjectCamp/Controllers/HeadingsController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Entity_Framework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,39 @@ namespace MvcProjectCamp.Controllers
     {
         // GET: Headings
         HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
         public ActionResult Index()
         {
             var headingValues = headingManager.GetAll();
             return View(headingValues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valueCategory = (from c in categoryManager.GetAll()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = c.CategoryName,
+                                                      Value = c.CategoryId.ToString()
+                                                  }).ToList();
+            ViewBag.valueCategory = valueCategory;
+
+            List<SelectListItem> valueWriter = (from w in writerManager.GetList()
+                                                select new SelectListItem
+                                                {
+                                                     Text = w.WriterName + " " + w.WriterSurName,
+                                                     Value = w.WriterId.ToString()
+                                                }).ToList();
+            ViewBag.valueWriter = valueWriter;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            headingManager.Add(heading);
+            return RedirectToAction("Index");
         }
     }
 }
