ecdbaac35cc522edd08350ba37f594a36f262d15
kubrasnmez <sonmezkbra@gmail.com>
2021-06-06 12:36:45 +0300

    gallery

---
+++ DataAccessLayer/Migrations/202106060741137_migrationAddImageFile.cs
@@ -0,0 +1,27 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class migrationAddImageFile : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ImageFiles",
+                c => new
+                    {
+                        ImageId = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 100),
+                        Path = c.String(maxLength: 250),
+                    })
+                .PrimaryKey(t => t.ImageId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ImageFiles");
+        }
+    }
+}
---
+++ EntityLayer/Concrete/ImageFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class ImageFile
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+        [StringLength(250)]
+        public string Path { get; set; }
+    }
+}
